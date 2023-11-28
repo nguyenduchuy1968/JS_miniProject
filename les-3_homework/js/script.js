021be5dd048c1ad4180cc6fac48f4b34f3041822
YOUR_NAME <YOUR_EMAIL>
2023-11-28 07:31:04 +0200

    Homework-3- edited

--- les-3_homework/js/script.js
+++ les-3_homework/js/script.js
@@ -34,41 +34,7 @@ while (i <= 20) {
     i++;
 }
 
-// ---------------------------------------------
-// 5-    є масив
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
-// за допомоги циклу вивести:
-// - користувачів зі статусом true
-for (const user of users) {
-    if (user.status) {
-        console.log(user);
-    }
-}
-
-console.log("---------------------------------");
-
-// - користувачів зі статусом false
-for (const user of users) {
-    if (!user.status) {
-        console.log(user);
-    }
-}
-
-console.log("---------------------------------");
-
-// 6 - Використовуючи данні з масиву, за допомоги document.write та циклу
+// 5 - Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
 // Масив:
 //
@@ -95,7 +61,7 @@ document.write(`</ul>`);
 
 document.write("---------------------------------");
 
-// 7- Використовуючи данні з масиву, за допомоги document.write та циклу
+// 6 - Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
 // Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
 //
@@ -138,10 +104,41 @@ for (const product of products) {
     document.write(`</div>`);
 }
 
+// ----------------------------------------------------------
 
+// 7 -    є масив
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+// за допомоги циклу вивести:
+// - користувачів зі статусом true
+for (const user of users) {
+    if (user.status) {
+        console.log(user);
+    }
+}
 
+console.log('--------------------------------------------');
+// - користувачів зі статусом false
+for (const user of users) {
+    if (!user.status) {
+        console.log(user);
+    }
+}
 
-// 8 - користувачів які старші за 30 років
+console.log('--------------------------------------------');
+
+// - користувачів які старші за 30 років
 for (const user of users) {
     if (user.age > 30) {
         console.log(user);
